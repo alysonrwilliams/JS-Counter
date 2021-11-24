f26ae66e921d3f543422ba262ffb130c2e3619d2
Alyson Williams <alysonrwilliams@gmail.com>
2021-11-23 21:12:53 -0500

    adding all files

---
+++ counter_files/app.js
@@ -0,0 +1,31 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        else if(styles.contains('increase')) {
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = "purple";
+        }
+        if(count < 0) {
+            value.style.color = "orange";
+        }
+        if(count === 0) {
+            value.style.color = "#777";
+        }
+        value.textContent = count;
+    })
+});
